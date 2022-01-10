9fd36ddae7747adc0c3a6fc52148be481fa3bf0f
Ricky Board <79424125+rboard321@users.noreply.github.com>
2022-01-10 15:50:20 -0500

    final

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { callApi } from "../utils";
-import { Link } from "react-router-dom";
+
+
 const { REACT_APP_API_URL } = process.env;
 
 const Routines = ({ setRoutines, routines, token, setToken }) => {
